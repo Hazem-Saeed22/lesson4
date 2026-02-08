da2b7fd3a44ab5ebf4598b9ab823c1c997cd5eab
Hazem <hazimsaid520@gmail.com>
2026-02-08 21:41:17 +0200

    second commit

--- node-lesson3/app.js
+++ node-lesson3/app.js
@@ -4,10 +4,17 @@ const port = 3001
 const mongoose = require('mongoose');
 app.use(express.urlencoded({ extended: true }));
 const Mydata = require("./modles/mydataSchema");
+app.set('view engine', 'ejs')
 
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname })
+
+  Mydata.find().then((result) => {
+     res.render("home", { mytitle: "home page", arr : result })
+  }).catch((err) => {
+    console.log(err);
+  });
+ 
 })
 app.get("/index.html", (req, res) => {
   res.send("<h1>تم الارسال بنجاح</h1>")
@@ -26,15 +33,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/all-data")
     console.log(err)
   });
 
-  app.post('/', (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body);
 
   const mydata = new Mydata(req.body);
   mydata.save().then(() => {
-   res.redirect("./index.html");
+    res.redirect("./index.html");
   }).catch((err) => {
     console.log(err);
   });
 
- 
+
 })
